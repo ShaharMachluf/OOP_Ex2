27557aba7886f2158200d873797b88a872e5ff61
shaimoo <92544909+shaimoo@users.noreply.github.com>
2021-11-30 18:28:58 +0200

    Add files via upload

--- OOP_2021/Assignments/Ex2/Tests/api/EdgeDataTest.java
+++ OOP_2021/Assignments/Ex2/Tests/api/EdgeDataTest.java
@@ -1,13 +1,13 @@
 package api;
 
-import src.My_EdgeDataImpl;
-import org.junit.Assert;
-import static  org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 class EdgeDataTest {
     int Src=10,Dest=0,tag=1;
     double weight=0.4;
     String Info = "blalalal";
-    Ex2.src.My_EdgeDataImpl test = new My_EdgeDataImpl(Src,Dest,weight,Info,tag);
+    My_EdgeDataImpl test = new My_EdgeDataImpl(Src,Dest,weight,Info,tag);
     @org.junit.jupiter.api.Test
     void getSrc() {
         assertEquals(test.getSrc(),Src);
@@ -19,19 +19,22 @@ class EdgeDataTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getWeight() {
+    void getWeight() {assertEquals((int) test.getWeight(),(int) weight);
     }
 
     @org.junit.jupiter.api.Test
-    void getInfo() {
+    void getInfo() {assertEquals(test.getInfo(),Info);
     }
 
     @org.junit.jupiter.api.Test
     void setInfo() {
+        test.setInfo("a");
+        assertEquals(test.getInfo(),"a");
     }
 
     @org.junit.jupiter.api.Test
-    void getTag() {
+    void getTag() {assertEquals(test.getTag(),tag);
+
     }
 
     @org.junit.jupiter.api.Test
