4349dd1ab374b4fed2ac7f0f66c67645dc0ac8e6
ShaharMachluf <85555432+ShaharMachluf@users.noreply.github.com>
2021-11-30 20:11:00 +0200

    Update My_DirectedWeightedGraphImpl.java

--- OOP_2021/Assignments/Ex2/src/api/My_DirectedWeightedGraphImpl.java
+++ OOP_2021/Assignments/Ex2/src/api/My_DirectedWeightedGraphImpl.java
@@ -2,11 +2,16 @@ package api;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Set;
 
 public class My_DirectedWeightedGraphImpl implements DirectedWeightedGraph {
     private int Edgecount=0,mc=0;
-    private boolean Iter=false;
+    private boolean NodeIter=false, EdgeIter = false;
     private HashMap <Integer,NodeData> Nodes = new HashMap<Integer, NodeData>();
+    /*for the edges we created two hashmaps of hashmaps, the Edgesrc hashmap's keys are the sources of the edges and
+    the values are hashmaps that their keys are the destinations of the edges coming out of the src key and their values
+    are the edges data. the Edgedest hashmap is the opposite.
+     */
     private HashMap <Integer,HashMap<Integer,EdgeData>> Edgesrc = new HashMap<Integer,HashMap<Integer, EdgeData>>();
     private HashMap <Integer,HashMap<Integer,EdgeData>> Edgedest = new HashMap<Integer,HashMap<Integer, EdgeData>>();
 
@@ -49,12 +54,18 @@ public class My_DirectedWeightedGraphImpl implements DirectedWeightedGraph {
 
     @Override
     public void addNode(NodeData n) {
+        if(this.NodeIter){
+            throw new RuntimeException();
+        }
         this.Nodes.put(n.getKey(),n);
         this.mc++;
     }
 
     @Override
     public void connect(int src, int dest, double w) {
+        if(NodeIter) {
+            throw new RuntimeException();
+        }
         EdgeData e = new My_EdgeDataImpl(src,dest,w);
         if(Edgesrc.containsKey(src))
             Edgesrc.get(src).put(dest,e);
@@ -70,18 +81,17 @@ public class My_DirectedWeightedGraphImpl implements DirectedWeightedGraph {
         {
             HashMap<Integer,EdgeData> p = new HashMap<Integer,EdgeData>();
             p.put(src,e);
-            Edgesrc.put(src,p);
+            Edgedest.put(src,p);
         }
+        this.Edgecount++;
         this.mc++;
     }
 
     @Override
     public Iterator<NodeData> nodeIter() {
-    if(Iter==false)
-    {
-
-    }
-
+        Iterator<NodeData> iter = Nodes.values().iterator();
+        this.NodeIter = true;
+        return iter;
     }
 
     @Override
@@ -91,31 +101,75 @@ public class My_DirectedWeightedGraphImpl implements DirectedWeightedGraph {
 
     @Override
     public Iterator<EdgeData> edgeIter(int node_id) {
-        return null;
+        Iterator<EdgeData> iter = Edgesrc.get(node_id).values().iterator();
+        this.EdgeIter = true;
+        return iter;
     }
 
     @Override
     public NodeData removeNode(int key) {
-        return null;
+        if(NodeIter){
+            throw new RuntimeException();
+        }
+        if(!Nodes.containsKey(key)){
+            return null;
+        }
+        NodeData n = getNode(key);
+        this.Nodes.remove(key);
+        //remove all the edges that come out of key node
+        Set<Integer> dests = Edgesrc.get(key).keySet();
+        Iterator<Integer> it = dests.iterator();
+        Edgedest.get(it).remove(key);
+        Edgecount--;
+        while(it.hasNext()){
+            Edgedest.get(it.next()).remove(key);
+            Edgecount--;
+            it.next();
+        }
+        Edgesrc.remove(key);
+        //remove all the edges that go into key node
+        Set<Integer> srcs = Edgedest.get(key).keySet();
+        it = srcs.iterator();
+        Edgesrc.get(it).remove(key);
+        Edgecount--;
+        while(it.hasNext()){
+            Edgesrc.get(it.next()).remove(key);
+            Edgecount--;
+            it.next();
+        }
+        Edgedest.remove(key);
+        this.mc++;
+        return n;
     }
 
     @Override
     public EdgeData removeEdge(int src, int dest) {
-        return null;
+        if(!Edgesrc.containsKey(src)){
+            return null;
+        }
+        if(NodeIter){
+            throw new RuntimeException();
+        }
+        EdgeData e = getEdge(src, dest);
+        this.Edgesrc.get(src).remove(dest);
+        this.Edgesrc.get(dest).remove(src);
+        this.Edgecount--;
+        this.mc++;
+        return e;
     }
 
     @Override
     public int nodeSize() {
-        return 0;
+        return this.Nodes.size();
     }
 
     @Override
     public int edgeSize() {
-        return 0;
+        return this.Edgecount;
     }
 
     @Override
     public int getMC() {
-        return 0;
+        return mc;
     }
 }
