a6084ebd1cffcabff3e6c122ea65a45487f27c0c
shaimoo <92544909+shaimoo@users.noreply.github.com>
2021-12-05 16:21:08 +0200

    Update My_DirectedWeightedGraphAlgorithmsImpl.java
    .

--- OOP_2021/Assignments/Ex2/src/api/My_DirectedWeightedGraphAlgorithmsImpl.java
+++ OOP_2021/Assignments/Ex2/src/api/My_DirectedWeightedGraphAlgorithmsImpl.java
@@ -1,17 +1,22 @@
 package api;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.w3c.dom.Node;
 
-import java.io.FileWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class My_DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGraphAlgorithms {
-   private  My_DirectedWeightedGraphImpl Graph ;
+    private  My_DirectedWeightedGraphImpl Graph ;
 
-   public  My_DirectedWeightedGraphAlgorithmsImpl()
-   {
-       this.Graph=new My_DirectedWeightedGraphImpl();
-   }
+    public  My_DirectedWeightedGraphAlgorithmsImpl()
+    {
+        this.Graph=new My_DirectedWeightedGraphImpl();
+    }
 
     @Override
     public void init(DirectedWeightedGraph g) {
@@ -26,7 +31,6 @@ public class My_DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedG
     @Override
     public DirectedWeightedGraph copy() {
         return new My_DirectedWeightedGraphImpl(this.Graph);
-
     }
 
     @Override
@@ -36,17 +40,133 @@ public class My_DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedG
 
     @Override
     public double shortestPathDist(int src, int dest) {
-        return 0;
+        List<NodeData> l = shortestPath(src,dest);
+        if(l == null){
+            return -1;
+        }
+        double pathSize = 0;
+        Iterator <NodeData> itSrc= l.listIterator();
+        Iterator <NodeData> itDest= l.listIterator();
+        itDest.next();
+        while(itDest.hasNext()){
+            EdgeData e =Graph.getEdge(itSrc.next().getKey(), itDest.next().getKey());
+            pathSize += e.getWeight();
+        }
+        return pathSize;
     }
 
     @Override
-    public List<NodeData> shortestPath(int src, int dest) {
-        return null;
+    public List<NodeData> shortestPath(int src, int dest) {//inspired by stackbuse.com/graphs-in-java-dijkstras-algorithms/
+        List <NodeData> pathList = new ArrayList<>();
+        int parent = Integer.MAX_VALUE;
+        HashMap<Integer,Integer> changed = new HashMap<>();
+        changed.put(src,null);
+        HashMap<Integer,Double> shortestPathMap = new HashMap<>();
+        Iterator <NodeData> it = Graph.nodeIter();
+        while(it.hasNext()){
+            NodeData n =it.next();
+            if(n.getKey() == src){
+                shortestPathMap.put(src,0.0);
+            }else{
+                shortestPathMap.put(n.getKey(),Double.POSITIVE_INFINITY);
+            }
+        }
+        Iterator <EdgeData> itE = Graph.edgeIter(src);
+        while(itE.hasNext()){
+            EdgeData e = itE.next();
+            shortestPathMap.put(e.getDest(),e.getWeight());
+            changed.put(e.getDest(),src);
+        }
+        Graph.getNode(src).setTag(1);//visited
+        while(true){
+            NodeData currNode = closestReachableUnvisited(shortestPathMap);
+            if(currNode == null){
+                return null;
+            }
+            if(currNode.getKey() == dest) {
+                int child = dest;
+                pathList.add(0,Graph.getNode(dest));
+                while (true) {
+                    parent = changed.get(child);
+                    if(parent == Integer.MAX_VALUE){
+                        break;
+                    }
+                    pathList.add(0,Graph.getNode(parent));
+                    child = parent;
+                }
+                pathList.add(0,Graph.getNode(src));
+                return pathList;
+            }
+            currNode.setTag(1);
+            Iterator <EdgeData> itEdges = Graph.edgeIter(currNode.getKey());
+            while(itEdges.hasNext()){
+                EdgeData e = itEdges.next();
+                if(Graph.getNode(e.getDest()).getTag() == 1){
+                    continue;
+                }
+                if(shortestPathMap.get(currNode.getKey()) + e.getWeight() < shortestPathMap.get(e.getDest())){
+                    shortestPathMap.put(e.getDest(), shortestPathMap.get(currNode.getKey())+e.getWeight());
+                    changed.put(e.getDest(), currNode.getKey());
+                }
+            }
+        }
+    }
+
+    private NodeData closestReachableUnvisited(HashMap<Integer,Double> ShortestPathMap){
+        double shortestDist = Double.POSITIVE_INFINITY;
+        NodeData closestReachableNode = null;
+        Iterator <NodeData> it = Graph.nodeIter();
+        while(it.hasNext()){
+            NodeData n = it.next();
+            if(n.getTag() == 1){
+                continue;
+            }
+            double currDist = ShortestPathMap.get(n.getKey());
+            if(currDist == Double.POSITIVE_INFINITY){
+                continue;
+            }
+            if(currDist < shortestDist){
+                shortestDist = currDist;
+                closestReachableNode = n;
+            }
+        }
+        return closestReachableNode;
     }
 
     @Override
     public NodeData center() {
-        return null;
+        int size = this.Graph.getNodes().size();
+        double min =  Double.MAX_VALUE;
+        double [][] matrix = new double[size][size];
+        double maxrow = 0,maxmin=Double.MAX_VALUE;
+        NodeData ans =null;
+        for (int i=0 ; i <size ;i++)
+        {
+            for(int j=0;j<size;j++) {
+                if(i==j)
+                    matrix[i][j]=Integer.MAX_VALUE;
+                matrix[i][j]=shortestPathDist(i,j);
+            }
+
+        }
+
+        for (int i=0 ; i <size ;i++)
+        {
+            for(int j=0;j<size;j++) {
+                if(i==j)
+                    continue;
+
+                if(maxrow<matrix[i][j])
+                    maxrow=matrix[i][j];
+            }
+
+            if(maxmin>maxrow) {
+                maxmin = maxrow;
+                ans=this.Graph.getNode(i);
+            }
+        }
+
+        return ans;
     }
 
     @Override
