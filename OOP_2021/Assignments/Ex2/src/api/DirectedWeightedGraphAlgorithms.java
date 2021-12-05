e62d404c3f77e9d775315ad20021f077307df983
shaimoo <92544909+shaimoo@users.noreply.github.com>
2021-12-05 15:40:59 +0200

    Add files via upload

--- OOP_2021/Assignments/Ex2/src/api/DirectedWeightedGraphAlgorithms.java
+++ OOP_2021/Assignments/Ex2/src/api/DirectedWeightedGraphAlgorithms.java
@@ -1,192 +1,93 @@
 package api;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.w3c.dom.Node;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
+/**
+ * This interface represents a Directed (positive) Weighted Graph Theory Algorithms including:
+ * 0. clone(); (copy)
+ * 1. init(graph);
+ * 2. isConnected(); // strongly (all ordered pais connected)
+ * 3. double shortestPathDist(int src, int dest);
+ * 4. List<NodeData> shortestPath(int src, int dest);
+ * 5. NodeData center(); // finds the NodeData which minimizes the max distance to all the other nodes.
+ *                       // Assuming the graph isConnected, elese return null. See: https://en.wikipedia.org/wiki/Graph_center
+ * 6. List<NodeData> tsp(List<NodeData> cities); // computes a list of consecutive nodes which go over all the nodes in cities.
+ *                                               // See: https://en.wikipedia.org/wiki/Travelling_salesman_problem
+ * 7. save(file); // JSON file
+ * 8. load(file); // JSON file
+ *
+ *
+ * @author boaz.benmoshe
+ *
+ */
+public interface DirectedWeightedGraphAlgorithms {
+    /**
+     * Inits the graph on which this set of algorithms operates on.
+     * @param g
+     */
+    public void init(DirectedWeightedGraph g);
 
-public class My_DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGraphAlgorithms {
-    private  My_DirectedWeightedGraphImpl Graph ;
+    /**
+     * Returns the underlying graph of which this class works.
+     * @return
+     */
+    public DirectedWeightedGraph getGraph();
+    /**
+     * Computes a deep copy of this weighted graph.
+     * @return
+     */
+    public DirectedWeightedGraph copy();
+    /**
+     * Returns true if and only if (iff) there is a valid path from each node to each
+     * other node. NOTE: assume directional graph (all n*(n-1) ordered pairs).
+     * @return
+     */
+    public boolean isConnected();
+    /**
+     * Computes the length of the shortest path between src to dest
+     * Note: if no such path --> returns -1
+     * @param src - start node
+     * @param dest - end (target) node
+     * @return
+     */
+    public double shortestPathDist(int src, int dest);
+    /**
+     * Computes the the shortest path between src to dest - as an ordered List of nodes:
+     * src--> n1-->n2-->...dest
+     * see: https://en.wikipedia.org/wiki/Shortest_path_problem
+     * Note if no such path --> returns null;
+     * @param src - start node
+     * @param dest - end (target) node
+     * @return
+     */
+    public List<NodeData> shortestPath(int src, int dest);
 
-    public  My_DirectedWeightedGraphAlgorithmsImpl()
-    {
-        this.Graph=new My_DirectedWeightedGraphImpl();
-    }
+    /**
+     * Finds the NodeData which minimizes the max distance to all the other nodes.
+     * Assuming the graph isConnected, elese return null. See: https://en.wikipedia.org/wiki/Graph_center
+     * @return the Node data to which the max shortest path to all the other nodes is minimized.
+     */
+    public NodeData center();
+   /**
+    * Computes a list of consecutive nodes which go over all the nodes in cities.
+    * the sum of the weights of all the consecutive (pairs) of nodes (directed) is the "cost" of the solution -
+    * the lower the better.
+    * See: https://en.wikipedia.org/wiki/Travelling_salesman_problem
+    */
+    List<NodeData> tsp(List<NodeData> cities);
+    /**
+     * Saves this weighted (directed) graph to the given
+     * file name - in JSON format
+     * @param file - the file name (may include a relative path).
+     * @return true - iff the file was successfully saved
+     */
+    public boolean save(String file);
 
-    @Override
-    public void init(DirectedWeightedGraph g) {
-        this.Graph=(My_DirectedWeightedGraphImpl) g;
-    }
-
-    @Override
-    public DirectedWeightedGraph getGraph() {
-        return this.Graph;
-    }
-
-    @Override
-    public DirectedWeightedGraph copy() {
-        return new My_DirectedWeightedGraphImpl(this.Graph);
-    }
-
-    @Override
-    public boolean isConnected() {
-        return false;
-    }
-
-    @Override
-    public double shortestPathDist(int src, int dest) {
-        List<NodeData> l = shortestPath(src,dest);
-        if(l == null){
-            return -1;
-        }
-        double pathSize = 0;
-        Iterator <NodeData> itSrc= l.listIterator();
-        Iterator <NodeData> itDest= l.listIterator();
-        itDest.next();
-        while(itDest.hasNext()){
-            EdgeData e =Graph.getEdge(itSrc.next().getKey(), itDest.next().getKey());
-            pathSize += e.getWeight();
-        }
-        return pathSize;
-    }
-
-    @Override
-    public List<NodeData> shortestPath(int src, int dest) {//inspired by stackbuse.com/graphs-in-java-dijkstras-algorithms/
-        List <NodeData> pathList = new ArrayList<>();
-        int parent = Integer.MAX_VALUE;
-        HashMap<Integer,Integer> changed = new HashMap<>();
-        changed.put(src,null);
-        HashMap<Integer,Double> shortestPathMap = new HashMap<>();
-        Iterator <NodeData> it = Graph.nodeIter();
-        while(it.hasNext()){
-            NodeData n =it.next();
-            if(n.getKey() == src){
-                shortestPathMap.put(src,0.0);
-            }else{
-                shortestPathMap.put(n.getKey(),Double.POSITIVE_INFINITY);
-            }
-        }
-        Iterator <EdgeData> itE = Graph.edgeIter(src);
-        while(itE.hasNext()){
-            EdgeData e = itE.next();
-            shortestPathMap.put(e.getDest(),e.getWeight());
-            changed.put(e.getDest(),src);
-        }
-        Graph.getNode(src).setTag(1);//visited
-        while(true){
-            NodeData currNode = closestReachableUnvisited(shortestPathMap);
-            if(currNode == null){
-                return null;
-            }
-            if(currNode.getKey() == dest) {
-                int child = dest;
-                pathList.add(0,Graph.getNode(dest));
-                while (true) {
-                    parent = changed.get(child);
-                    if(parent == Integer.MAX_VALUE){
-                        break;
-                    }
-                    pathList.add(0,Graph.getNode(parent));
-                    child = parent;
-                }
-                pathList.add(0,Graph.getNode(src));
-                return pathList;
-            }
-            currNode.setTag(1);
-            Iterator <EdgeData> itEdges = Graph.edgeIter(currNode.getKey());
-            while(itEdges.hasNext()){
-                EdgeData e = itEdges.next();
-                if(Graph.getNode(e.getDest()).getTag() == 1){
-                    continue;
-                }
-                if(shortestPathMap.get(currNode.getKey()) + e.getWeight() < shortestPathMap.get(e.getDest())){
-                    shortestPathMap.put(e.getDest(), shortestPathMap.get(currNode.getKey())+e.getWeight());
-                    changed.put(e.getDest(), currNode.getKey());
-                }
-            }
-        }
-    }
-
-    private NodeData closestReachableUnvisited(HashMap<Integer,Double> ShortestPathMap){
-        double shortestDist = Double.POSITIVE_INFINITY;
-        NodeData closestReachableNode = null;
-        Iterator <NodeData> it = Graph.nodeIter();
-        while(it.hasNext()){
-            NodeData n = it.next();
-            if(n.getTag() == 1){
-                continue;
-            }
-            double currDist = ShortestPathMap.get(n.getKey());
-            if(currDist == Double.POSITIVE_INFINITY){
-                continue;
-            }
-            if(currDist < shortestDist){
-                shortestDist = currDist;
-                closestReachableNode = n;
-            }
-        }
-        return closestReachableNode;
-    }
-
-    @Override
-    public NodeData center() {
-        return null;
-    }
-
-    @Override
-    public List<NodeData> tsp(List<NodeData> cities) {
-        return null;
-    }
-
-    @Override
-    public boolean save(String file) {
-        try {
-            Gson gson = new Gson();
-            Writer writer = new FileWriter(file);
-            gson.toJson(this.Graph, writer);
-            writer.close();
-            return true;
-        }
-        catch (Exception a)
-        {
-            return false;
-        }
-    }
-
-    @Override
-    public boolean load(String file) {
-        try {
-            Gson gson = new Gson();
-            FileReader reader =new FileReader(file);
-            HashMap<String, List<HashMap>> h = gson.fromJson(reader, HashMap.class);
-            List <HashMap> nodes = h.get("Nodes");
-            Iterator <HashMap> it = nodes.iterator();
-            while(it.hasNext()){
-                HashMap n = it.next();
-                int key = Integer.parseInt((String) n.get("id"));
-                String[] location = ((String)n.get("pos")).split(",");
-                GeoLocation g = new My_GeoLocation(Double.parseDouble(location[0]), Double.parseDouble(location[1]), Double.parseDouble(location[2]));
-                Graph.addNode(new My_NodeData(key, g));
-            }
-            List <HashMap> edges = h.get("Edges");
-            it = edges.iterator();
-            while(it.hasNext()){
-                HashMap e = it.next();
-                int src = Integer.parseInt((String) e.get("src"));
-                int dest = Integer.parseInt((String) e.get("dest"));
-                double weight = Double.parseDouble((String) e.get("w"));
-                Graph.connect(src, dest, weight);
-            }
-            reader.close();
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+    /**
+     * This method loads a graph to this graph algorithm.
+     * if the file was successfully loaded - the underlying graph
+     * of this class will be changed (to the loaded one), in case the
+     * graph was not loaded the original graph should remain "as is".
+     * @param file - file name of JSON file
+     * @return true - iff the graph was successfully loaded.
+     */
+    public boolean load(String file);
 }
