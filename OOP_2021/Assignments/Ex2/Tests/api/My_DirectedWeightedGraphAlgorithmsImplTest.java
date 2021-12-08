0b39c75e2141f65ae0a6ab9977358eebe9d24352
ShaharMachluf <85555432+ShaharMachluf@users.noreply.github.com>
2021-12-08 17:52:01 +0200

    Update My_DirectedWeightedGraphAlgorithmsImplTest.java

--- OOP_2021/Assignments/Ex2/Tests/api/My_DirectedWeightedGraphAlgorithmsImplTest.java
+++ OOP_2021/Assignments/Ex2/Tests/api/My_DirectedWeightedGraphAlgorithmsImplTest.java
@@ -1,13 +1,16 @@
 package api;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class My_DirectedWeightedGraphAlgorithmsImplTest {
-    public  My_DirectedWeightedGraphAlgorithmsImpl test  ;
+    My_DirectedWeightedGraphAlgorithmsImpl test = new My_DirectedWeightedGraphAlgorithmsImpl();
+
     @Test
     void init() {
         DirectedWeightedGraphAlgorithms a = new My_DirectedWeightedGraphAlgorithmsImpl();
@@ -95,29 +98,62 @@ class My_DirectedWeightedGraphAlgorithmsImplTest {
         a.init(g2);
         a.getGraph().connect(n3.getKey(), n1.getKey(), 3);
         assertTrue(a.isConnected());
+        test.load("C:/Users/shaha/IdeaProjects/OOP_Ex2/OOP_2021/Assignments/Ex2/data/G2.json");
+        assertTrue(test.isConnected());
     }
 
     @Test
     void shortestPathDist() {
+        DirectedWeightedGraphAlgorithms a = new My_DirectedWeightedGraphAlgorithmsImpl();
+        DirectedWeightedGraph g1 = new My_DirectedWeightedGraphImpl();
+        NodeData n1 = new My_NodeData(1, new My_GeoLocation(1.0,1.0,0.0));
+        NodeData n2 = new My_NodeData(2, new My_GeoLocation(2.0,2.0,0.0));
+        NodeData n3 = new My_NodeData(3, new My_GeoLocation(3.0,3.0,0.0));
+        g1.addNode(n1);
+        g1.addNode(n2);
+        g1.addNode(n3);
+        g1.connect(n1.getKey(),n2.getKey(),1.2);
+        g1.connect(n2.getKey(), n3.getKey(),3.4);
+        g1.connect(n1.getKey(), n3.getKey(),5);
+        a.init(g1);
+        double dist = a.shortestPathDist(n1.getKey(), n3.getKey());
+        assertEquals(dist, 4.6);
     }
- @Test
+
+    @Test
     void shortestPath() {
-        test.load("C:/Users/shaim/IdeaProjects/OOP_Ex2/OOP_2021/Assignments/Ex2/data/G1.json");
-        NodeData ans = test.getGraph().getNode(2);
-        List <NodeData> a = test.shortestPath(2,3);
-        assertEquals(ans.getKey(),a.get(0).getKey());
-        ans =test.getGraph().getNode(3);
-        assertEquals(ans.getKey(),a.get(1).getKey());
+//        test.load("C:/Users/shaha/IdeaProjects/OOP_Ex2/OOP_2021/Assignments/Ex2/data/G1.json");
+//        NodeData ans = test.getGraph().getNode(2);
+//        List<NodeData> a = test.shortestPath(2,3);
+//        assertEquals(ans.getKey(),a.get(0).getKey());
+//        ans =test.getGraph().getNode(3);
+//        assertEquals(ans.getKey(),a.get(1).getKey());
+        test.load("C:/Users/shaha/IdeaProjects/OOP_Ex2/OOP_2021/Assignments/Ex2/data/G1.json");
+        NodeData ans = test.getGraph().getNode(3);
+        List<NodeData> a= test.shortestPath(0,3);
+        assertEquals(ans.getKey(), a.get(3).getKey());
     }
+
     @Test
     void center() {
+        test.load("C:/Users/shaha/IdeaProjects/OOP_Ex2/OOP_2021/Assignments/Ex2/data/G2.json");
+        NodeData Center = test.center();
+        assertEquals(0,Center.getKey());
     }
 
     @Test
     void tsp() {
+        test.load("C:/Users/shaha/IdeaProjects/OOP_Ex2/OOP_2021/Assignments/Ex2/data/G1.json");
+        DirectedWeightedGraph g = test.getGraph();
+        List <NodeData> cities = new ArrayList<>();
+        for(int i=0;i<6;i++){
+            cities.add(g.getNode(i));
+        }
+        List <NodeData> ans = test.tsp(cities);
+        assertEquals(cities.get(1).getKey(), 1);
     }
 
-     @Test
+    @Test
     void save() {
         test.load("C:/Users/shaim/IdeaProjects/OOP_Ex2/OOP_2021/Assignments/Ex2/data/G1.json");
         test.getGraph().removeEdge(16,0);
